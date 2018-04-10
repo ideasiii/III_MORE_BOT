393f6fdc1d2b446072296d079f81bef245bd3041
ideasiii <louisju@iii.org.tw>
2018-04-10 16:35:10 +0800

    init

---
+++ app/src/main/java/org/iii/iii_more_bot/MainActivity.java
@@ -0,0 +1,15 @@
+package org.iii.iii_more_bot;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
