1fb58504a5208ad973fc6ddb669ca04994ccb38c
ideasiii <louisju@iii.org.tw>
2018-04-16 12:26:18 +0800

    jugo update

--- app/src/main/java/org/iii/iii_more_bot/MainActivity.java
+++ app/src/main/java/org/iii/iii_more_bot/MainActivity.java
@@ -1,3 +1,12 @@
+/****************************************************************
+ This is a garbage project.
+ The project will build a robot.
+ All modules are like garbage.
+ So do not use this application.
+ You will cry..........
+ Date:2018-04-16
+ ****************************************************************/
+
 package org.iii.iii_more_bot;
 
 import android.support.v7.app.AppCompatActivity;
@@ -5,7 +14,6 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity
 {
-
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
